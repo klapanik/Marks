d7c46d3ecf9d5f2848d08ad5245112500bcd79d2
Kostik Lapanik <lapanik.kostik@gmail.com>
2025-09-21 18:03:38 +0300

    feat: add basic aliases

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,16 @@
+import { dirname, resolve } from "path"
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { fileURLToPath } from "url"
+
+const __dirname = dirname(fileURLToPath(import.meta.url))
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      "@": resolve(__dirname, "./src"),
+    },
+  },
 })
