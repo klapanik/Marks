f4b7718c1767487d587273fd7a186b93f01e1b44
Kostik Lapanik <lapanik.kostik@gmail.com>
2025-10-10 13:39:13 +0300

    install tailwind css

--- vite.config.ts
+++ vite.config.ts
@@ -2,12 +2,13 @@ import { dirname, resolve } from "path"
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { fileURLToPath } from "url"
+import tailwindcss from '@tailwindcss/vite'
 
 const __dirname = dirname(fileURLToPath(import.meta.url))
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
       "@": resolve(__dirname, "./src"),
