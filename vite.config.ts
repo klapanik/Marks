6f295b20f8883c97a9ea3ae4f0c4a2eea2ddcd47
Kostik Lapanik <lapanik.kostik@gmail.com>
2025-10-10 13:45:57 +0300

    install shadcnui

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,13 @@
-import { dirname, resolve } from "path"
+import path from "path"
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { fileURLToPath } from "url"
 import tailwindcss from '@tailwindcss/vite'
 
-const __dirname = dirname(fileURLToPath(import.meta.url))
-
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
-      "@": resolve(__dirname, "./src"),
+      "@": path.resolve(__dirname, "./src"),
     },
   },
 })
