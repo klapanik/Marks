d7da311aeab0bd0d472cdced408e011efd676fb8
Kostik Lapanik <lapanik.kostik@gmail.com>
2025-10-17 20:16:20 +0300

    add tooltip for the sidebar trigger button

--- src/widgets/AppSidebar/AppSidebar.tsx
+++ src/widgets/AppSidebar/AppSidebar.tsx
@@ -10,12 +10,22 @@ import {
     SidebarMenuButton,
     SidebarMenuItem,
     SidebarTrigger,
-} from "@/components/ui/sidebar"
+} from "@/components/ui/sidebar";
+
+import {
+    Tooltip,
+    TooltipContent,
+    TooltipTrigger,
+} from "@/components/ui/tooltip";
+
+import { Kbd, KbdGroup } from "@/components/ui/kbd";
+
 import { House, BookOpenText, Calendar, GraduationCap, CalendarDays, School } from 'lucide-react';
-import { NavLink } from "react-router-dom"
+import { NavLink } from "react-router-dom";
 
 import type { LinkType } from "@/shared/models";
 import { LogoutBtn } from "@/shared/ui/LogoutBtn";
+import { TooltipProvider } from "@radix-ui/react-tooltip";
 
 const links: LinkType[] = [
     { title: 'Главная', url: '/home', icon: House },
@@ -28,8 +38,25 @@ const links: LinkType[] = [
 export function AppSidebar() {
     return (
         <div className="relative h-full">
-            <SidebarTrigger className=" transition-colors duration-300 hover:bg-smoky-white 
-                absolute -right-9 top-4 cursor-pointer" />
+            <TooltipProvider>
+                <Tooltip>
+                    <TooltipTrigger asChild>
+                        <SidebarTrigger className="transition-colors duration-300 hover:bg-smoky-white 
+                        absolute -right-9 top-4 cursor-pointer" />
+                    </TooltipTrigger>
+                    <TooltipContent
+                        side="left"
+                        className="flex items-center gap-2 p-2"
+                    >
+                        <span className="text-sm text-primary">Боковая панель</span>
+                        <KbdGroup>
+                            <Kbd>Ctrl</Kbd>
+                            <span>+</span>
+                            <Kbd>B</Kbd>
+                        </KbdGroup>
+                    </TooltipContent>
+                </Tooltip>
+            </TooltipProvider>
 
             <Sidebar>
                 <div className="px-4 py-5 relative h-full">
