6fd1d6779a541ad90b2ef6c4a420e311507f7a6a
Kostik Lapanik <lapanik.kostik@gmail.com>
2025-10-18 15:25:00 +0300

    feat: improve architecture of sidebar, add separate component StatisticsGroup

--- src/pages/MainPage/MainPage.tsx
+++ src/pages/MainPage/MainPage.tsx
@@ -1,18 +1,11 @@
-import { Stat } from "@/shared/ui/Stat";
-import { Calendar, CalendarDays, TrendingUp } from "lucide-react";
 import { HelloBlock } from "./HelloBlock/HelloBlock";
+import { StatisticsGroup } from "./StatisticGroup/StatisticsGroup";
 
 export function MainPage() {
     return (
         <section>
             <HelloBlock />
-
-            <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-4">
-                <Stat title="Сегодня уроков" icon={CalendarDays} number={0} subtext="Выходной день" />
-                <Stat title="Оценки" icon={TrendingUp} number={0} subtext="за последние время" />
-                <Stat title="Средний балл" icon={TrendingUp} number={9} subtext="за всё время" />
-                <Stat title="Текущая неделя" icon={Calendar} number={1} subtext="учебного года" />
-            </div>
+            <StatisticsGroup />
         </section>
     )
 }
