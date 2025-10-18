a77f1612c451aa86b3f8b401dae96926093dba6d
Kostik Lapanik <lapanik.kostik@gmail.com>
2025-10-18 15:16:58 +0300

    feat: add hello block, styles for it

--- src/pages/MainPage/MainPage.tsx
+++ src/pages/MainPage/MainPage.tsx
@@ -1,10 +1,12 @@
 import { Stat } from "@/shared/ui/Stat";
 import { Calendar, CalendarDays, TrendingUp } from "lucide-react";
+import { HelloBlock } from "./HelloBlock/HelloBlock";
 
 export function MainPage() {
     return (
         <section>
-            <div></div>
+            <HelloBlock />
+
             <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-4">
                 <Stat title="Сегодня уроков" icon={CalendarDays} number={0} subtext="Выходной день" />
                 <Stat title="Оценки" icon={TrendingUp} number={0} subtext="за последние время" />
