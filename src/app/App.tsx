a30278a9ee4292fbf20548ff8e43cdfd5dbd3303
Kostik Lapanik <lapanik.kostik@gmail.com>
2025-10-25 15:56:55 +0300

    init: start working at subject page

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -2,12 +2,15 @@ import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } 
 
 import { RootLayout } from "@/layouts/RootLayout";
 import { MainPage } from "@/pages/MainPage/MainPage";
+import { SubjectsPage } from "@/pages/SubjectsPage/SubjectsPage";
 
 export function App() {
     const router = createBrowserRouter(createRoutesFromElements(
         <Route path="/" element={<RootLayout />}>
             <Route index element={<MainPage />} />
             <Route path="/home" element={<MainPage />} />
+
+            <Route path="/subjects" element={<SubjectsPage />} />
         </Route>
     ));
 
---
+++ src/pages/SubjectsPage/SubjectsPage.tsx
@@ -0,0 +1,14 @@
+export function SubjectsPage() {
+    return (
+        <section>
+            <div>
+                <h2 className="font-bold text-3xl">Предметы</h2>
+                <p className="text-muted-foreground">Выберите предметы, которые вы изучаете</p>
+            </div>
+
+            {/* <YourSubjects /> */}
+            {/* <AddNewSubject /> */}
+            {/* <BasicSubjects /> */}
+        </section>
+    )
+}
