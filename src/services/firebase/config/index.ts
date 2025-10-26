25b0bdae85e6bd5a76a8c4021c220ab52eb22df6
Kostik Lapanik <lapanik.kostik@gmail.com>
2025-10-26 10:09:29 +0300

    feat: install firebase, add basic firebase config

---
+++ src/services/firebase/config/index.ts
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyA_cLx_Jg3Xp4_L8UJ4dHeunQAYsm_DsRM",
+    authDomain: "marks-71838.firebaseapp.com",
+    projectId: "marks-71838",
+    storageBucket: "marks-71838.firebasestorage.app",
+    messagingSenderId: "73826927915",
+    appId: "1:73826927915:web:e7960553b8a58d4b83716e",
+    measurementId: "G-RVCYDBNTS5"
+};
+
+const app = initializeApp(firebaseConfig);
