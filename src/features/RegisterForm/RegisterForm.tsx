f42b76dd7be177fcc035468a527e78483189dc49
Kostik Lapanik <lapanik.kostik@gmail.com>
2025-11-21 18:28:09 +0300

    feat: add full ui of register form

--- src/features/RegisterForm/RegisterForm.tsx
+++ src/features/RegisterForm/RegisterForm.tsx
@@ -29,6 +29,118 @@ export function RegisterForm({ onSubmit }: Props) {
     return (
         <Form {...form}>
             <form onSubmit={handleSubmit(onSubmit)} className='flex flex-col gap-5 my-2'>
+                <div className='flex gap-2'>
+                    <FormField
+                        control={control}
+                        name="name"
+                        render={({ field }) => (
+                            <FormItem>
+                                <FormLabel>Имя</FormLabel>
+                                <FormControl>
+                                    <Input
+                                        className={`primary-input ${errors.name ? 'invalid' : ''}`}
+                                        placeholder="Имя"
+                                        {...field}
+                                    />
+                                </FormControl>
+                                <FormMessage className={`${errors.surname ? 'min-h-5' : ''}`}>{errors.name?.message && ''} </FormMessage>
+                            </FormItem>
+                        )}
+                    />
+
+                    <FormField
+                        control={control}
+                        name="surname"
+                        render={({ field }) => (
+                            <FormItem>
+                                <FormLabel>Фамилия</FormLabel>
+                                <FormControl>
+                                    <Input
+                                        className={`primary-input ${errors.surname ? 'invalid' : ''}`}
+                                        placeholder="Фамилия"
+                                        {...field}
+                                    />
+                                </FormControl>
+                                <FormMessage className={`${errors.name ? 'min-h-5' : ''}`}>{errors.surname?.message && ''} </FormMessage>
+                            </FormItem>
+                        )}
+                    />
+                </div>
+
+                <div className='flex gap-2'>
+                    <FormField
+                        control={control}
+                        name="form"
+                        render={({ field }) => (
+                            <FormItem>
+                                <FormLabel>Класс</FormLabel>
+                                <FormControl>
+                                    <Input
+                                        type='number'
+                                        className={`primary-input ${errors.form ? 'invalid' : ''}`}
+                                        placeholder="Класс"
+                                        {...field}
+                                    />
+                                </FormControl>
+                                <FormMessage className={`${errors.letter ? 'min-h-5' : ''}`}>{errors.form?.message && ''} </FormMessage>
+                            </FormItem>
+                        )}
+                    />
+
+                    <FormField
+                        control={control}
+                        name="letter"
+                        render={({ field }) => (
+                            <FormItem>
+                                <FormLabel>Буква</FormLabel>
+                                <FormControl>
+                                    <Input
+                                        className={`primary-input ${errors.letter ? 'invalid' : ''}`}
+                                        placeholder="Буква"
+                                        {...field}
+                                    />
+                                </FormControl>
+                                <FormMessage className={`${errors.letter?.message ? 'min-h-5' : ''}`}>{errors.letter?.message && ''} </FormMessage>
+                            </FormItem>
+                        )}
+                    />
+                </div>
+
+                <FormField
+                    control={control}
+                    name="email"
+                    render={({ field }) => (
+                        <FormItem>
+                            <FormLabel>Email</FormLabel>
+                            <FormControl>
+                                <Input
+                                    className={`primary-input ${errors.email ? 'invalid' : ''}`}
+                                    placeholder="your@email.com"
+                                    {...field}
+                                />
+                            </FormControl>
+                            <FormMessage>{errors.email?.message && ''}</FormMessage>
+                        </FormItem>
+                    )}
+                />
+
+                <FormField
+                    control={control}
+                    name="password"
+                    render={({ field }) => (
+                        <FormItem>
+                            <FormLabel>Пароль</FormLabel>
+                            <FormControl>
+                                <Input
+                                    className={`primary-input ${errors.password ? 'invalid' : ''}`}
+                                    placeholder="Создайте пароль"
+                                    {...field}
+                                />
+                            </FormControl>
+                            <FormMessage>{errors.password?.message && ''}</FormMessage>
+                        </FormItem>
+                    )}
+                />
 
                 <Button
                     type="submit"
