4a48ac6f3ce9567bf66b17cafc0f6eca16e5e41e
Kostik Lapanik <lapanik.kostik@gmail.com>
2025-11-21 18:28:39 +0300

    feat: add full schema for register form

--- src/features/RegisterForm/zod/index.ts
+++ src/features/RegisterForm/zod/index.ts
@@ -2,10 +2,13 @@ import { loginFormSchema } from '@/features/LoginForm/zod';
 import { z } from 'zod';
 
 export const registerFormSchema = loginFormSchema.extend({
-    name: z.string('Введите своё имя'),
-    surname: z.string('Введите свою фамилию'),
-    form: z.string('Введите ваш класс'),
-    letter: z.string('Введите букву вашего класса')
+    name: z.string('Введите своё имя').regex(/^\p{L}+$/u, 'Можно вводить только буквы').min(2, 'Введите минимум 2 символа'),
+    surname: z.string('Введите свою фамилию').regex(/^\p{L}+$/u, 'Можно вводить только буквы'),
+    form: z.string('Введите ваш класс').min(1, 'Введите ваш класс').regex(/^(1[0-2]|[1-9])$/, 'Введите число от 1 до 13'),
+    letter: z.string('Введите букву вашего класса').max(1, 'Введите одну букву')
+        .regex(/^$|^\p{L}$/u, 'Можно вводить только буквы')
+        .transform(val => val.toUpperCase())
+        .optional(),
 })
 
 export type RegisterFormType = z.infer<typeof registerFormSchema>;
