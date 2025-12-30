f62b62d21ce70fb3c6acd5fee0085ef7e85765bd
Kostik Lapanik <lapanik.kostik@gmail.com>
2025-12-30 15:11:19 +0300

    feat: add confirm alert for loging out

--- src/shared/ui/LogoutBtn.tsx
+++ src/shared/ui/LogoutBtn.tsx
@@ -1,19 +1,59 @@
-import { Button } from "@/components/ui/button";
 import { firebaseAuthService } from "@/services/firebase/auth";
+
 import { LogOut } from "lucide-react";
+
+import {
+    AlertDialog,
+    AlertDialogAction,
+    AlertDialogCancel,
+    AlertDialogContent,
+    AlertDialogDescription,
+    AlertDialogFooter,
+    AlertDialogHeader,
+    AlertDialogTitle,
+    AlertDialogTrigger,
+} from "@/components/ui/alert-dialog";
+import { Button } from "@/components/ui/button";
 
 export function LogoutBtn() {
     async function sighOut() {
         firebaseAuthService.signOutUser();
     }
 
-    // Todo: add confirm alert
-
     return (
-        <Button variant="ghost" size="sm" className="w-full justify-start cursor-pointer 
-            transition-colors duration-500 hover:bg-white" onClick={sighOut}>
-            <LogOut className="mr-3" />
-            <span>Выйти</span>
-        </Button>
-    )
+        <AlertDialog>
+            <AlertDialogTrigger asChild>
+                <Button
+                    variant="ghost"
+                    size="sm"
+                    className="w-full justify-start cursor-pointer transition-colors duration-500 hover:bg-white"
+                >
+                    <LogOut className="mr-3" />
+                    <span>Выйти</span>
+                </Button>
+            </AlertDialogTrigger>
+
+            <AlertDialogContent className="bg-white">
+                <AlertDialogHeader>
+                    <AlertDialogTitle>Вы уверены, что хотите выйти из аккаунта?</AlertDialogTitle>
+                    <AlertDialogDescription>
+                        Если вы выйдите из аккаунта, вы не потеряете свою учетную запись и всё равно
+                        сможете войти в аккаунт позже
+                    </AlertDialogDescription>
+                </AlertDialogHeader>
+
+                <AlertDialogFooter>
+                    <AlertDialogCancel className="cursor-pointer text-white hover:text-white">
+                        Назад
+                    </AlertDialogCancel>
+                    <AlertDialogAction
+                        className="cursor-pointer text-white hover:text-white"
+                        onClick={sighOut}
+                    >
+                        Продолжить
+                    </AlertDialogAction>
+                </AlertDialogFooter>
+            </AlertDialogContent>
+        </AlertDialog>
+    );
 }
